ae36376fd9e35bbc5ff0abce4d850b9ad98ebfee
Mor-gan <ogundipeabimbola9@gmail.com>
2023-10-28 14:54:25 +0100

    changed file sorce path

--- src/main/java/com/cbfacademy/JSONFileHandler.java
+++ src/main/java/com/cbfacademy/JSONFileHandler.java
@@ -14,6 +14,7 @@ public class JSONFileHandler {
 
     public static List<Employee> readFile(String filePath) {
         List<Employee> employees = new ArrayList<>();
+        filePath = "/Users/bimbo/Desktop/cbfacademy/java-json-exercise-Mor-gan/src/main/resources/example.json";
         try {
             File sourceFile = Paths.get(filePath).toFile();
             ObjectMapper objectMapper = new ObjectMapper();
@@ -27,11 +28,20 @@ public class JSONFileHandler {
 
     public static void save(Employee employee, String outputFile) {
         // Add code to save the employee to the specified output file
+        outputFile="/Users/bimbo/Desktop/cbfacademy/java-json-exercise-Mor-gan/src/main/java/com/cbfacademy/Outputfile.java";
+        List<Employee> employees = new ArrayList<>();
+        try {
+            employees.add(employee);
+            File sourceFile = Paths.get(outputFile).toFile();
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(sourceFile, employee);
+            System.out.println("File created at: " + outputFile);
+            ;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
-        String filePath = "/Users/bimbo/Desktop/cbfacademy/java-json-exercise-Mor-gan/src/main/java/com/cbfacademy/Employee/Employee.java";
-        List<Employee> employees = readFile(filePath);
-
     }
 }
